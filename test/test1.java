bd277fdd54d0281c3b9f51c8fda45d81fea3ccca
deirwin <deirwin@gmail.com>
2016-12-01 10:29:54 -0500

    More modifications.

--- test/test1.java
+++ test/test1.java
@@ -2,7 +2,7 @@ public class test1 {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, Worl");
+        System.out.println("Hello, World");
     }
 
 }
